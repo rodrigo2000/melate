b52332a5c8f2183d4964e04b8db3e305c1ae7359
Rodrigo Alejandro Sevilla Blanco <rodrigosevilla2000@hotmail.com>
2015-10-06 15:53:21 -0500

    Versión 1.0 del controlador

---
+++ application/controllers/Panel.php
@@ -0,0 +1,63 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Panel extends MY_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->table_name = "";
+        $this->module['name'] = 'panel';
+        $this->module['folder'] = '';
+        $this->module['controller'] = 'Panel';
+        $this->id_field = "";
+    }
+
+    function index() {
+        $this->load->view("template");
+    }
+
+    function getArchivoMelateFormURL() {
+        $url = $this->input->post("url");
+        //El nombre del archivo donde se almacenara los datos descargados.
+        $filePath = realpath(".") . implode(DIRECTORY_SEPARATOR, array('','resources', 'MelateNuevo.csv'));
+        $file = fopen($filePath, "w+");
+        $result = array(
+            'success' => FALSE,
+            'mensaje' => ''
+        );
+        // Crea un nuevo recurso cURL
+        $ch = curl_init();
+
+        // Establece la URL y otras opciones apropiadas
+        curl_setopt($ch, CURLOPT_URL, $url);
+        //Si necesitamos el header del archivo, en este caso no.
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+        //Si necesitamos descargar el archivo.
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        //Lee el header y se mueve a la siguiente localización.
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+        //Cantidad de segundo de limite para conectarse.
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+        //Cantidad de segundos de limite para ejecutar curl. 0 significa indefinido.
+        curl_setopt($ch, CURLOPT_TIMEOUT, 0);
+        //Donde almacenaremos el archivo.
+        curl_setopt($ch, CURLOPT_FILE, $file);
+
+        // Captura la URL y la envía al navegador
+        $r = curl_exec($ch);
+
+        // Cierrar el recurso cURLy libera recursos del sistema
+        curl_close($ch);
+
+        if ($r) {
+            $result['mensaje'] = "Descarga exitosa";
+        } else {
+            $result['mensaje'] = "CURL Error: #" . curl_errno($ch) . " " . curl_error($ch);
+        }
+        echo json_encode($result);
+    }
+
+}
